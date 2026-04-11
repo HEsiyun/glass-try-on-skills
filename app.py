d315a236a372af25cdaef3d49ea3e1b1b277244c
Siyun He <hesiyun0603@gmail.com>
2026-04-11 00:26:30 -0700

    Fix mediapipe import: use explicit path instead of mp.solutions

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ model_path = 'face_detection_yunet_2023mar.onnx'
 face_detector = cv2.FaceDetectorYN.create(model_path, "", (320, 320))
 
 # Face mesh
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
+from mediapipe.python.solutions.face_mesh import FaceMesh
+face_mesh = FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
 
 # Glasses state
 num = 1
